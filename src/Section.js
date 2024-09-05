d444ea72bc8fb5e9479f8f13bb6d2a917b3a63af
Anurag Verma <mindreaders7557@gmail.com>
2024-09-05 14:09:09 +0530

    Update Section.js

--- src/Section.js
+++ src/Section.js
@@ -1,7 +1,7 @@
 const button = document.querySelector("#getstarted a button");
 let width = windows.innerWidth;
 
-function changeText() {
+export default function changeText() {
   if(width < 480) {
       button.textContent = "Get API";
   }
